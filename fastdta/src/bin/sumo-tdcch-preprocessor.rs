70f4b35c0e0e6a11e629e6263a8d7426cae9aac5
henrikcs <henrik.csoere@protonmail.com>
2025-07-03 23:05:22 +0200

    refactor: call inertialflowcutter explicitly

--- fastdta/src/bin/sumo-tdcch-preprocessor.rs
+++ fastdta/src/bin/sumo-tdcch-preprocessor.rs
@@ -21,6 +21,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let output_dir = args.output_dir.unwrap_or(String::from(env::current_dir()?.to_str().unwrap()));
 
+    dbg!(&input_dir);
+    dbg!(&input_prefix);
     dbg!(&output_dir);
 
     let input_dir = Path::new(&input_dir);
@@ -45,16 +47,66 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // create a subprocess which runs the bash script: "flow_cutter_cch_cut_order.sh <output_dir>" to create node rankings for the TD-CCH
 
-    let output_dir_str = output_dir.to_str().ok_or("Output directory is not valid UTF-8")?;
+    run_inertial_flow_cutter_console(output_dir, args.seed.unwrap_or(5489), args.routing_threads.unwrap_or(-1))?;
 
-    // make sure that this project's root directory is in the PATH, such that flow_cutter_cch_cut_order.sh can be found
-    let status = std::process::Command::new("bash")
-        .arg("flow_cutter_cch_cut_order.sh")
-        .arg(output_dir_str)
+    Ok(())
+}
+
+fn run_inertial_flow_cutter_console(directory: &Path, seed: i32, threads: i32) -> Result<(), Box<dyn Error>> {
+    // make sure that "console" is in the PATH (i.e. lib/InertialFlowCutter/build/console)
+    // the values have been copied from flow_cutter_cch_cut_order.sh
+
+    let status = std::process::Command::new("console")
+        .arg("load_routingkit_unweighted_graph")
+        .arg(directory.join("first_out").to_str().unwrap())
+        .arg(directory.join("head").to_str().unwrap())
+        .arg("load_routingkit_longitude")
+        .arg(directory.join("longitude").to_str().unwrap())
+        .arg("load_routingkit_latitude")
+        .arg(directory.join("latitude").to_str().unwrap())
+        .arg("flow_cutter_set")
+        .arg("random_seed")
+        .arg(seed.to_string())
+        .arg("reorder_nodes_at_random")
+        .arg("reorder_nodes_in_preorder")
+        .arg("flow_cutter_set")
+        .arg("thread_count")
+        .arg(threads.to_string())
+        .arg("flow_cutter_set")
+        .arg("BulkDistance")
+        .arg("no")
+        .arg("flow_cutter_set")
+        .arg("max_cut_size")
+        .arg("100000000")
+        .arg("flow_cutter_set")
+        .arg("distance_ordering_cutter_count")
+        .arg("0")
+        .arg("flow_cutter_set")
+        .arg("geo_pos_ordering_cutter_count")
+        .arg("8")
+        .arg("flow_cutter_set")
+        .arg("bulk_assimilation_threshold")
+        .arg("0.4")
+        .arg("flow_cutter_set")
+        .arg("bulk_assimilation_order_threshold")
+        .arg("0.25")
+        .arg("flow_cutter_set")
+        .arg("bulk_step_fraction")
+        .arg("0.05")
+        .arg("flow_cutter_set")
+        .arg("initial_assimilated_fraction")
+        .arg("0.05")
+        .arg("flow_cutter_config")
+        .arg("report_time")
+        .arg("reorder_arcs_in_accelerated_flow_cutter_cch_order")
+        .arg("normal")
+        .arg("do_not_report_time")
+        .arg("save_routingkit_arc_permutation_since_last_load")
+        .arg(directory.join("cch_perm_cuts").to_str().unwrap())
         .status()?;
 
     if !status.success() {
-        panic!("Failed to run flow_cutter_cch_cut_order.sh script");
+        return Err(Box::from("Failed to run Inertial Flow Cutter console command"));
     }
 
     Ok(())
