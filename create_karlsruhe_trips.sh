4ee76514816f17b4e8c0f04805218d74c15e0e1e
henrikcs <henrik.csoere@protonmail.com>
2025-07-24 11:25:07 +0200

    fix: add paths to dependencies

--- create_karlsruhe_trips.sh
+++ create_karlsruhe_trips.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env sh
+declare spack_env=$1
 
-SUMO_TOOLS=~/rust_road_router/venvs/libsumo/lib/python3.11/site-packages/sumo/tools python create_trips.py import/sumo/karlsruhe/karlsruhe.net.xml import/sumo/karlsruhe karlsruhe
+declare pwd=$(pwd)
+
+P=$(basename $(find ~/.user_spack/environments/"$spack_env"/.spack-env/._view -mindepth 1 -maxdepth 1 -type d))
+export PATH="$pwd"/lib/InertialFlowCutter/build:"$pwd"/target/debug:~/rust-nightly/bin:~/.local/bin:$PATH
+export RL=$(spack location -i readline%gcc@14)
+export NC=$(spack location -i ncurses%gcc@14)
+export LX=$(spack location -i libx11%gcc@14)
+export LIBRARY_PATH=$LX/lib:$RL/lib:$NC/lib:$LIBRARY_PATH
+export CPATH=$RL/include:$NC/include:$LX/include
+export LD_LIBRARY_PATH=~/.local/libnsl1/lib64:~/.user_spack/environments/"$spack_env"/.spack-env/._view/"$P"/lib:$NC/lib:$LD_LIBRARY_PATH
+
+export SUMO_TOOLS=~/rust_road_router/venvs/libsumo/lib/python3.11/site-packages/sumo/tools 
+
+python create_trips.py import/sumo/karlsruhe/karlsruhe.net.xml import/sumo/karlsruhe karlsruhe
