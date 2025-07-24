5d83a48ee9b07a825be8f39f776734def1be24c6
henrikcs <henrik.csoere@protonmail.com>
2025-07-24 11:00:04 +0200

    build: create script for calling create_trips.py to be run on the benchmark server

---
+++ create_karlsruhe_trips.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+SUMO_TOOLS=~/rust_road_router/venvs/libsumo/lib/python3.11/site-packages/sumo/tools python create_trips.py import/sumo/karlsruhe/karlsruhe.net.xml import/sumo/karlsruhe karlsruhe
