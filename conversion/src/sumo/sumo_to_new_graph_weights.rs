6a75028a72c4b12c01dfb9aa14c5043fd4e436af
henrikcs <henrik.csoere@protonmail.com>
2025-09-15 14:04:51 +0200

    refactor: rename method and remove unused code

--- conversion/src/sumo/sumo_to_new_graph_weights.rs
+++ conversion/src/sumo/sumo_to_new_graph_weights.rs
@@ -25,14 +25,14 @@ pub fn get_graph_with_travel_times_from_previous_iteration(input_dir: &Path, ite
     if iteration > 0 {
         let previous_iteration_dir = input_dir.join(format!("{:0>3}", iteration - 1));
 
-        extract_travel_times_from_previous_iteration(&previous_iteration_dir, &input_dir, &edge_ids);
+        extract_travel_times_from_iteration_directory(&previous_iteration_dir, &input_dir, &edge_ids);
     }
 
     // TODO: instead of reconstructing the graph from a file, we could create it in memory
     TDGraph::reconstruct_from(&input_dir).expect("Failed to reconstruct the time-dependent graph")
 }
 
-pub fn extract_travel_times_from_previous_iteration(previous_iteration_dir: &Path, path_to_graph_weights: &Path, edge_indices_to_id: &Vec<String>) {
+pub fn extract_travel_times_from_iteration_directory(previous_iteration_dir: &Path, path_to_graph_weights: &Path, edge_indices_to_id: &Vec<String>) {
     // dump file starts with "dump_" and ends with ".xml"
     let dump_file = get_meandata_file(&previous_iteration_dir);
 
