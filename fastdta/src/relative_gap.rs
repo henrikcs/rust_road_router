7a15aa5744f0dea0724d20011b30695227c687ac
henrikcs <henrik.csoere@protonmail.com>
2025-09-21 11:47:03 +0200

    refactor: print error message if simulated time is less than best time

--- fastdta/src/relative_gap.rs
+++ fastdta/src/relative_gap.rs
@@ -15,16 +15,13 @@ pub fn get_relative_gap(best_tt: &Vec<f64>, simulated_tt: &Vec<f64>) -> f64 {
                 return 0.0;
             }
 
-            // if simulated_tt[i] < tt {
-            //     return 0.0;
-            // }
-            debug_assert!(
-                simulated_tt[i] - tt >= -EPSILON_TRAVEL_TIME,
-                "Simulated travel time for trip {} is less than best travel time: {} < {}",
-                i,
-                simulated_tt[i],
-                tt
-            );
+            if simulated_tt[i] < tt {
+                eprintln!(
+                    "Simulated travel time for trip {} is less than best travel time: {} < {}",
+                    i, simulated_tt[i], tt
+                );
+                return 0.0;
+            }
 
             simulated_tt[i] - tt
         })
