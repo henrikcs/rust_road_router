5597b4227c3209f98f9b2c9be9728f03f8aa5f20
henrikcs <henrik.csoere@protonmail.com>
2025-09-22 11:59:23 +0200

    fix: add special case of a single edge to dijkstra router

--- fastdta/src/query.rs
+++ fastdta/src/query.rs
@@ -49,6 +49,12 @@ pub fn get_paths_with_dijkstra_queries(
     get_paths_from_queries(
         |from_edge, to_edge, from: u32, to: u32, departure: Timestamp, graph: &TDGraph| {
             let from_edge_tt = graph.get_travel_time_along_path(departure, &[from_edge]);
+
+            if from_edge == to_edge {
+                // special case: from and to are the same edge
+                return Some((vec![from_edge], from_edge_tt));
+            }
+
             let delayed_departure = departure + from_edge_tt;
 
             let result = query_server.td_query(TDQuery {
