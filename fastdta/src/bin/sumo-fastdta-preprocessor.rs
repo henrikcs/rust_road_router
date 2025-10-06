bb12107654e88a57f5ce649526487b30378ab7ba
henrikcs <henrik.csoere@protonmail.com>
2025-10-06 18:36:28 +0200

    feature: implement prototype of fastdta routing

---
+++ fastdta/src/bin/sumo-fastdta-preprocessor.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+use std::path::Path;
+
+use clap::Parser;
+use conversion::sumo::sumo_to_td_graph_converter::convert_sumo_to_routing_kit_and_queries;
+use fastdta::cli;
+use fastdta::preprocess::{preprocess, run_inertial_flow_cutter};
+use rust_road_router::report::measure;
+
+/// has the following parameters:
+/// - input_dir: the directory containing the input files
+/// - input_prefix: the prefix of the input files
+/// - output_dir: the directory to write the output files to (optional, defaults to current directory)
+/// - seed: the random seed to use for the inertial flow cutter (optional, defaults to 5489)
+/// - routing_threads: the number of threads to use for the routing
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = cli::PreprocesserArgs::parse();
+
+    let input_dir = Path::new(&args.input_dir);
+    let input_prefix = args.input_prefix;
+    let trips_file = Path::new(&args.trips_file);
+    let output_dir = Path::new(&args.output_dir);
+
+    let (_, duration) = measure(|| convert_sumo_to_routing_kit_and_queries(&input_dir, &input_prefix, &trips_file, &output_dir));
+    log(&output_dir.display().to_string(), "preprocessing", duration.as_nanos());
+
+    // create a subprocess which runs the bash script: "flow_cutter_cch_cut_order.sh <output_dir>" to create node rankings for the TD-CCH
+    let (_, duration) = measure(|| run_inertial_flow_cutter(&output_dir, args.seed, args.routing_threads));
+    log(&output_dir.display().to_string(), "inertial flow cutter", duration.as_nanos());
+
+    // run catchup preprocessing
+    let (_, duration) = measure(|| preprocess(&output_dir));
+    log(&output_dir.display().to_string(), "cch preprocessing", duration.as_nanos());
+
+    Ok(())
+}
+
+/// Logs the operation with the duration in nanoseconds within a certain iteration of certain run identified by identifier.
+/// The format is: "sumo-tdcch-preprocessor; <identifier>; <iteration>; <operation>; <duration_in_nanos>"
+fn log(identifier: &str, operation: &str, duration_in_nanos: u128) {
+    println!("sumo-tdcch-preprocessor; {}; -1; {}; {}", identifier, operation, duration_in_nanos);
+}
--- fastdta/src/bin/sumo-fastdta-router.rs
+++ fastdta/src/bin/sumo-fastdta-router.rs
@@ -7,7 +7,7 @@ use fastdta::cli;
 use fastdta::cli::Parser;
 use fastdta::customize::customize;
 use fastdta::preprocess::get_cch;
-use fastdta::query::get_paths_with_cch_queries;
+use fastdta::query::{get_paths_with_cch_queries, read_queries};
 use rust_road_router::algo::catchup::Server;
 use rust_road_router::io::read_strings_from_file;
 use rust_road_router::report::measure;
@@ -23,7 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     assert!(args.max_alternatives > 0, "max_alternatives must be greater than 0");
 
-    let ((edge_ids, graph, cch), duration) = measure(|| {
+    let ((edge_ids, graph, cch, query_data, old_paths), duration) = measure(|| {
         let edge_ids: Vec<String> = read_strings_from_file(&input_dir.join(FILE_EDGE_INDICES_TO_ID)).unwrap_or_else(|_| {
             panic!(
                 "Failed to read edge indices from file {} in directory {}",
@@ -33,24 +33,75 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         });
         let graph = get_graph_with_travel_times_from_previous_iteration(input_dir, iteration, &edge_ids);
         let cch = get_cch(input_dir, &graph);
+        let query_data = read_queries(input_dir);
 
-        (edge_ids, graph, cch)
+        // paths, which have been used the previous iteration
+        let old_paths: Vec<Vec<u32>> = Vec::new();
+
+        (edge_ids, graph, cch, query_data, old_paths)
     });
     log(&input_dir.display().to_string(), iteration, "preprocessing", duration.as_nanos());
 
-    // this is basically doing a one-shot assignment
+    let (customized_graph, duration) = measure(|| customize(&cch, &graph));
+    log(&input_dir.display().to_string(), iteration, "cch customization", duration.as_nanos());
+
     // customize with previous travel times
     // while not all trips have been sampled:
     //   sample a subset of trips
     //   find shortest routes for the sampled trips
     //   customize the graph with the shortest routes using the density on the edges during a time window
 
-    let (customized_graph, duration) = measure(|| customize(&cch, &graph));
-    log(&input_dir.display().to_string(), iteration, "cch customization", duration.as_nanos());
+    // there should be two samples:
+    // 1. every 10th query
+    // 2. the remaining queries
 
-    let ((shortest_paths, travel_times, departures), duration) =
-        measure(|| get_paths_with_cch_queries(&mut Server::new(&cch, &customized_graph), &input_dir, &graph));
-    log(&input_dir.display().to_string(), iteration, "cch routing", duration.as_nanos());
+    let samples: Vec<Vec<usize>> = vec![
+        (0..query_data.0.len()).step_by(10).collect(),             // every 10th query
+        (0..query_data.0.len()).filter(|i| i % 10 != 0).collect(), // the remaining queries
+    ];
+
+    let mut shortest_paths: Vec<Vec<u32>> = Vec::with_capacity(query_data.0.len());
+    let mut travel_times = Vec::with_capacity(query_data.0.len());
+    let mut departures = Vec::with_capacity(query_data.0.len());
+
+    for (i, sample) in samples.iter().enumerate() {
+        let ((sampled_shortest_paths, sampled_travel_times, sampled_departures), duration) = measure(|| {
+            get_paths_with_cch_queries(
+                &mut Server::new(&cch, &customized_graph),
+                &sample.iter().map(|&i| query_data.0[i]).collect(),
+                &sample.iter().map(|&i| query_data.1[i]).collect(),
+                &sample.iter().map(|&i| query_data.2[i]).collect(),
+                &sample.iter().map(|&i| query_data.3[i]).collect(),
+                &sample.iter().map(|&i| query_data.4[i]).collect(),
+                &graph,
+            )
+        });
+        log(
+            &input_dir.display().to_string(),
+            iteration,
+            format!("cch routing (sample {i})").as_str(),
+            duration.as_nanos(),
+        );
+
+        sample.iter().for_each(|&i| {
+            shortest_paths.insert(i, sampled_shortest_paths[i].clone());
+            travel_times.insert(i, sampled_travel_times[i]);
+            departures.insert(i, sampled_departures[i]);
+        });
+
+        //TODO: update the graph's travel times according to the changed vehicle flows on the edges
+        // graph = update_edge_capacities(graph, &old_paths, &new_paths, &departures);
+
+        let (_, duration) = measure(|| {
+            customize(&cch, &graph);
+        });
+        log(
+            &input_dir.display().to_string(),
+            iteration,
+            format!("cch customization (sample {i})").as_str(),
+            duration.as_nanos(),
+        );
+    }
 
     let write_sumo_alternatives =
         args.no_write_sumo_alternatives == "false" || args.no_write_sumo_alternatives == "0" || args.no_write_sumo_alternatives == "False";
