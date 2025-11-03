ab1b782113aa6e24e663b6219bf0730d1a54e9c0
henrikcs <henrik.csoere@protonmail.com>
2025-11-03 15:20:33 +0100

    test: temporatily remove relative gap calculation to investigate problem

--- run_experiments.sh
+++ run_experiments.sh
@@ -193,7 +193,6 @@ while IFS=';' read -r in_dir prefix trip_file_name aggregation convergence_devia
                 --mesosim --aggregation "$aggregation" --begin 0 --end 86400 -l $last_iter
                 --routing-algorithm "$algorithm"
                 --max-convergence-deviation "$convergence_deviation"
-                --relative-gap "$convergence_relgap"
                 --logit
                 --logitbeta
                 1.0
@@ -208,8 +207,6 @@ while IFS=';' read -r in_dir prefix trip_file_name aggregation convergence_devia
                 sumo--aggregate-warnings 5
                 sumo--seed $seed
                 sumo--precision 6
-                relative-gap--net-prefix "$prefix"
-                relative-gap--net-dir "$in_dir"
             )
 
             # Add preprocessor args only for CCH
