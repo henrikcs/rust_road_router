94c2a6a81e0f6bc9c4ad3de5ffd1e83213942ac9
henrikcs <henrik.csoere@protonmail.com>
2025-11-04 19:45:02 +0100

    debug: reintroduce relative gap calculation

--- run_experiments.sh
+++ run_experiments.sh
@@ -193,7 +193,7 @@ while IFS=';' read -r in_dir prefix trip_file_name aggregation convergence_devia
                 --mesosim --aggregation "$aggregation" --begin 0 --end 86400 -l $last_iter
                 --routing-algorithm "$algorithm"
                 --max-convergence-deviation "$convergence_deviation"
-                # --relative-gap "$convergence_relgap"
+                --relative-gap "$convergence_relgap"
                 --logit
                 --logitbeta
                 1.0
@@ -211,8 +211,8 @@ while IFS=';' read -r in_dir prefix trip_file_name aggregation convergence_devia
                 sumo--precision 9
                 sumo--step-length 0.1
                 # sumo--threads $(nproc)
-                # relative-gap--net-prefix "$prefix"
-                # relative-gap--net-dir "$in_dir"
+                relative-gap--net-prefix "$prefix"
+                relative-gap--net-dir "$in_dir"
             )
 
             # Add preprocessor args only for CCH
