df84693076d80cd420ca7484b183fa2eee49a2bc
henrikcs <henrik.csoere@protonmail.com>
2025-11-06 14:51:01 +0100

    debug: set min edge weight to a smaller value for less interference with travel time calculation

--- conversion/src/sumo/sumo_to_td_graph_converter.rs
+++ conversion/src/sumo/sumo_to_td_graph_converter.rs
@@ -51,7 +51,7 @@ impl FlattenedSumoEdge {
 }
 
 /// lowest possible travel time for an edge in seconds
-pub const MIN_EDGE_WEIGHT: f64 = 0.5;
+pub const MIN_EDGE_WEIGHT: f64 = 0.125;
 
 impl Clone for FlattenedSumoEdge {
     fn clone(&self) -> Self {
