deaf7f821fdcb1caa90211463a13a7d65901ce80
henrikcs <henrik.csoere@protonmail.com>
2025-11-06 18:51:26 +0100

    debug: min edge weight

--- conversion/src/sumo/sumo_to_td_graph_converter.rs
+++ conversion/src/sumo/sumo_to_td_graph_converter.rs
@@ -51,7 +51,7 @@ impl FlattenedSumoEdge {
 }
 
 /// lowest possible travel time for an edge in seconds
-pub const MIN_EDGE_WEIGHT: f64 = 0.125;
+pub const MIN_EDGE_WEIGHT: f64 = 1.0;
 
 impl Clone for FlattenedSumoEdge {
     fn clone(&self) -> Self {
