9f0c1d926ef450675443cb1ed1ff578b487ef858
henrikcs <henrik.csoere@protonmail.com>
2026-01-30 18:43:16 +0100

    fix: use spack env

--- run_experiments.sh
+++ run_experiments.sh
@@ -262,14 +262,14 @@ EOF
 # declare release_type="release"
 # declare spack_env="fast-dta"
 declare pwd=$(pwd)
-P=$(basename $(find ~/.user_spack/environments/fast-dta/.spack-env/._view -mindepth 1 -maxdepth 1 -type d))
+P=$(basename $(find ~/.user_spack/environments/$spack_env/.spack-env/._view -mindepth 1 -maxdepth 1 -type d))
 export PATH="$pwd"/lib/InertialFlowCutter/build:"$pwd"/target/"$release_type":~/rust-nightly/bin:~/.local/bin:$PATH
 export RL=$(spack location -i readline%gcc@14)
 export NC=$(spack location -i ncurses%gcc@14)
 export LX=$(spack location -i libx11%gcc@14)
 export LIBRARY_PATH=$LX/lib:$RL/lib:$NC/lib:$LIBRARY_PATH
 export CPATH=$RL/include:$NC/include:$LX/include:$CPATH
-export LD_LIBRARY_PATH=~/.local/libnsl1/lib64:~/.user_spack/environments/fast-dta/.spack-env/._view/"$P"/lib:$NC/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=~/.local/libnsl1/lib64:~/.user_spack/environments/$spack_env/.spack-env/._view/"$P"/lib:$NC/lib:$LD_LIBRARY_PATH
 export SUMO_HOME=~/rust_road_router/venvs/libsumo/lib/python3.11/site-packages/sumo
 export PATH=$SUMO_HOME/bin:$PATH
 
